1144cb8525fd68868a1a2e97acab78244e9bf6b7
EvanCarver-10 <835238@mystma.org>
2022-09-16 16:58:42 -0400

    4

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
-    return a, b, c, d, e, 
-
-print(topFiveMovies("End of Evangelion", "Evangelion 1.0", "Evangelion 2.0", "Evangelion 3.0 + 1.0", "Evangelion 3.0"))
+    return a, b, c, d, e
